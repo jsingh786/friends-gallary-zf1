ceeffde79b04bcfcff4cd6fa4f8c98b7bab9b869
kaurharjinder <kaurharjinder@seasiainfotech.com>
2016-09-28 13:09:24 +0530

    I am working on album/index

---
+++ public/js/frontend/album.js
@@ -0,0 +1,33 @@
+$(function(){
+    $("#but").on("click", function(){
+        
+        var offset= 0;
+        var limit=9;
+
+$.ajax({
+        url : "/" + user-management-zf1 + "album/index",
+        method : "POST",
+        data : {"limit":limit,"offset": offset},
+        type : "post",
+        dataType : "json",
+        success : function(data) {
+            //Do your stuff aftee success.
+                console.log(data)
+            //     var resultdata=data;
+            //     var count = resultdata.length-1;
+
+            //     for(i=1;i<=count;i++) {
+            //     $("#main").append('<div class="Colomn"><a href="#"><img src="" class="Img" ><div class="Info-album"><b>Name:</b>'
+            //         +resultdata[i].Name'<br><b>Location:</b>'
+            //         +resultdata[i].Location'<br><b>Description:</b>'
+            //         +resultdata[i].Description'<br><b>Created At:</b>'
+            //         +resultdata[i].CreatedAt'<br></div></a></div>')
+            // }
+            //     var off= parseInt($("button#but").attr("offset"));
+            //     off+=limit;
+            //     $("button#but").attr("offset", off);
+        }
+    });
+
+});
+});
