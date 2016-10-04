f80f3f9fd43cb6784e12bb0c511e9cbe86be6630
kaurharjinder <kaurharjinder@seasiainfotech.com>
2016-10-04 15:04:51 +0530

    Worked to make albums dynamic

--- public/js/frontend/album.js
+++ public/js/frontend/album.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $("#but").on("click", function(){
            var offset= 0;
         var limit=9;
-    $.ajax({
+});    
 
 });
 function loadAlbums(limit, offset, user_id)
@@ -21,7 +21,7 @@ function loadAlbums(limit, offset, user_id)
         success : function(result,status,xhr) {
 
             //HTML
-            HTML = '';
+            HTML = '<div id = "main">';
             HTML += '<div class = "Row">';
 
             for(i in result)
@@ -29,16 +29,16 @@ function loadAlbums(limit, offset, user_id)
                 //Add check that when (i+1) is divisible by 3 and has no remainder, then close the row div tag and start new row div.
                 if((i+1)%3 == 0)
                 {
+                    $("div#main").append(HTML);
                     HTML += '</div>';
                     HTML += '<div class = "Row">';
                 }
 
-                HTML = '';
-
+                HTML = '<div class="Colomn"><a href="#"><img src="" class="Img" ><div class="Info-album"><b>Name:'+result[i].id+result[i].name+'</b><br><b>Location:'+result[i].location+'</b><br><b>Description:'+result[i].description+'</b><br><b>Created At:'+result[i].created_at+'</b><br>'
                 //console.log(result[i].id+result[i].name);
-
             }
-            $("div#main").append(HTML);
+            //alert("Name: " + result[i].id);
+           // $("div#main").append(HTML);
         }
     });
 }
